f4947445f372c900e3ec0ed6c1c70899bbb8b002
Samarth-Parnami <samarth.p@beaconstac.com>
2024-01-16 23:34:06 +0530

    revert wrong migration

--- django_POC/app1/migrations/0002_mymodel_age.py
+++ django_POC/app1/migrations/0002_mymodel_age.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app1', '0001_initial'),
+        ('app1', '0002_initial'),
     ]
 
     operations = [
