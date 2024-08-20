6cc94bb710f07b0f0054651be733edf57ebb1219
shivam tiwari <shivam.t@mobstac.com>
2024-08-20 12:48:56 +0530

    test commits

--- django_POC/app1/migrations/0003_test_field2.py
+++ django_POC/app1/migrations/0003_test_field2.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('app1', '0001_initial'),
+        ('app1', '0002_test_field'),
     ]
 
     operations = [
