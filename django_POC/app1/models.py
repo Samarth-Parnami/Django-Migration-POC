4f6f998ba6a6be96512fdeadd5cc5dedf8003784
shivam tiwari <shivam.t@mobstac.com>
2024-08-20 12:44:46 +0530

    test fields

--- django_POC/app1/models.py
+++ django_POC/app1/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class MyModel(models.Model):
     name = models.CharField(max_length=10)
     test_field = models.BooleanField(default=False, null=False, blank=False)
+    test_field2 = models.BooleanField(default=False, null=False, blank=False)
